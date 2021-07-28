6c42a2ae7ed074b64106c2129484252095259112
J/A <archer884@zoho.com>
2021-07-27 21:23:28 -0500

    Use an iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,42 @@
 use std::io::{self, Read};
 
-pub struct CatRead<T, F> {
-    source: F,
+pub struct CatRead<T, S> {
+    source: S,
     current: T,
 }
 
-impl<T, F> CatRead<T, F>
+impl<T, S> CatRead<T, S>
 where
     T: Read,
-    F: FnMut() -> Option<io::Result<T>>,
+    S: Iterator<Item = io::Result<T>>,
 {
-    pub fn new(mut source: F) -> io::Result<Self> {
-        let current = source()
-            .transpose()?
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "no sources"))?;
+    pub fn new<I>(source: I) -> io::Result<Self>
+    where
+        I: IntoIterator<IntoIter = S>,
+    {
+        let mut source = source.into_iter();
+        let current = source
+            .next()
+            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "no sources"))??;
         Ok(Self { source, current })
     }
 }
 
-impl<T, F> Read for CatRead<T, F>
+impl<T, S> Read for CatRead<T, S>
 where
     T: Read,
-    F: FnMut() -> Option<io::Result<T>>,
+    S: Iterator<Item = io::Result<T>>,
 {
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         let mut bytes_read = 0;
 
         loop {
             match self.current.read(&mut buf[bytes_read..]) {
                 // Change belts or surrender
-                Ok(0) => match (self.source)() {
+                Ok(0) => match self.source.next() {
                     Some(source) => self.current = source?,
                     None => return Ok(bytes_read),
-                }
+                },
 
                 // Keep firing, assholes!
                 Ok(len) => {
@@ -58,8 +62,8 @@ mod tests {
     #[test]
     fn it_works() {
         let sources = &["Hello, ", "world!"];
-        let mut sources = sources.iter().map(Cursor::new);
-        let catread = CatRead::new(|| sources.next().map(Ok)).unwrap();
+        let sources = sources.iter().map(Cursor::new);
+        let catread = CatRead::new(sources.map(Ok)).unwrap();
         let actual = read_to_string(catread).unwrap();
         assert_eq!(actual, "Hello, world!");
     }
